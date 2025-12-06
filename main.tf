8c61aa38949f2f25656a382ffcc05542890b45d0
Dialgatrainer02 <oliviabarker900@gmail.com>
2025-12-06 13:10:07 +0000

    toset

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "proxmox_virtual_environment_vm" "proxmox_vm" {
 # disks
 
 dynamic "disk" {
-  for_each = var.proxmox_vm_clone != null ? []: ["1"]
+  for_each = toset(var.proxmox_vm_clone != null ? []: ["1"])
   content {
     # disable if clone not null
    import_from = proxmox_virtual_environment_download_file.proxmox_vm_boot_image["1"].id
@@ -151,7 +151,7 @@ locals {
 }
 
 resource "proxmox_virtual_environment_download_file" "proxmox_vm_boot_image" {
-  for_each = var.proxmox_vm_clone != null ? []: ["1"]
+  for_each = toset(var.proxmox_vm_clone != null ? []: ["1"])
   # disable if clone not null
   content_type = var.proxmox_vm_boot_image.content_type
   datastore_id = local.boot_image_datastore_id
